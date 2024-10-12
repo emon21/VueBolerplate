f46e0ac326d6ab0965a6d427aaba6d826419dca4
emon21 <emonbd66@gmail.com>
2024-10-12 18:57:48 +0600

    feat:First Vue Project

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+
+import MenuItem from './components/MenuItem.vue'
+
+</script>
+
+
+<template>
+
+
+  <!-- <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it! Vue 3" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main> -->
+
+  <!-- <div></div> -->
+
+  <MenuItem />
+  <div></div>
+
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+
+ 
+
+
+  <!-- <h2 class="text-3xl font-bold underline text-red-500">
+    Hello Vue Application</h2> -->
+
+  <!-- <p class="text-red-500" style="font-size: 24px;color: blue;">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, consectetur? Aliquam dignissimos mollitia a,
+    dolorem dolores voluptate ullam magni, aspernatur cum hic consequatur? Mollitia, laboriosam enim quasi repudiandae
+    explicabo quis?
+  </p>  -->
+
+
+
+
+
+</template>
+
+<style scoped>
+/* 
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+ 
+} */
+</style>
---
+++ src/components/MenuItem.vue
@@ -0,0 +1,82 @@
+
+// Menu Item
+
+<script>
+import { defineComponent } from 'vue'
+ 
+ export default defineComponent({})
+
+</script>
+
+<template>
+   <div class="menu">
+      <ul>
+         <li> <a href="#">Home</a></li>
+         <li> <a href="#">About US</a></li>
+         <li> <a href="#">News</a></li>
+         <li> <a href="#">Blog</a></li>
+         <li> <a href="#">Service</a></li>
+         <li> <a href="#">Protfolio</a></li>
+         <li> <a href="#">Contact US</a></li>
+      </ul>
+   </div>
+</template>
+
+
+<style>
+.menu{
+   /* background-color: bisque; */
+   width: 120%;
+   /* margin: 0 auto; */
+   margin-bottom: 50px;
+}
+ul {
+   list-style: none;
+   padding: 0;
+   margin: 0;
+   /* display: flex; */
+   /* flex-direction: row; */
+   /* display: inline-block; */
+}
+
+ul li {
+   /* padding: 10px; */
+   margin: 0 2px;
+   background: tomato;
+   /* background: blue; */
+   display: inline-block;
+   /* display: inline-flex; */
+   border-top-left-radius: 5px;
+   border-bottom-right-radius: 5px;
+   /* border: 2px solid blue; */
+}
+
+ul li:last-child {
+   /* border-right: none; */
+
+   /* border-top-left-radius: 0; */
+   /* border-top-right-radius: 5px; */
+}
+
+ul li a {
+   text-decoration: none;
+   color: black;
+   padding: 10px;
+   font-size: 20px;
+   display: inline-block;
+   text-align: center;
+   /* display: block; */
+   transition: all 0.5s ease-in-out;
+   /* padding-top: 25px; */
+}
+
+ul li a:hover {
+   color: white;
+   /* padding: 10px; */
+   /* transform: translate(0,5px); */
+   transform: scale(1.2);
+   background-color: transparent;
+}
+
+
+</style>
